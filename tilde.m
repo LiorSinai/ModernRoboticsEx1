944217a56b7a69be65aee23b63e2be66a21a9b9b
Lior Sinai <sinailior>
2018-06-10 22:16:11 +0200

    New file

---
+++ tilde.m
@@ -0,0 +1,13 @@
+%%% Lior Sinai and Juan Pablo Avelar Luna
+%%% 10 June 2018
+%%% Calculate skew function of a vector
+
+function xtilde= tilde(x)
+
+    %if all(size(x)~=([3 1]))
+    %    error('input vector is not a 3x1 vector')
+    %end
+
+    xtilde=[0 -x(3) x(2); x(3) 0 -x(1); -x(2) x(1) 0; ] ;
+
+end
