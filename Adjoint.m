6a5d207475b07202ffcdb1676ead44df32c31d04
Lior Sinai <sinailior>
2018-06-12 15:54:12 +0200

    Created a new Adjoint function

---
+++ Adjoint.m
@@ -0,0 +1,12 @@
+%%% Lior Sinai and Juan Pablo Avelar Luna
+%%% 10 June 2018
+%%% Calculate the Adjoint matrix of a homogenous H matrix
+
+function Adj = Adjoint(H)
+    
+    R=H(1:3,1:3);
+    p=H(1:3,4);
+    
+    Adj=[R zeros(3,3); tilde(p)*R R]
+
+end
