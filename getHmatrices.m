9b637a09fc5f565d031a5f4a966524a4efb321ec
Lior Sinai <sinailior>
2018-06-13 17:27:21 +0200

    More name corrections

--- getHmatrices.m
+++ getHmatrices.m
@@ -21,9 +21,9 @@ function [H0_1, H0_2, H0_3] = getHmatrices(q, L)
     H0_3_0=[eye(3) [0 0 L(1)+L(2)+L(3)]' ; 0 0 0 1];
     
    % Calculate the H matrices
-    H0_1 = expTwist(T100,q(1))*H1_0_0;
-    H0_2 = expTwist(T100,q(1))*expTwist(T210,q(2))*H2_0_0;
-    H0_3 = expTwist(T100,q(1))*expTwist(T210,q(2))*expTwist(T320,q(3))*H3_0_0; 
+    H0_1 = expTwist(T100,q(1))*H0_1_0;
+    H0_2 = expTwist(T100,q(1))*expTwist(T210,q(2))*H0_2_0;
+    H0_3 = expTwist(T100,q(1))*expTwist(T210,q(2))*expTwist(T320,q(3))*H0_3_0; 
 
 end
 
