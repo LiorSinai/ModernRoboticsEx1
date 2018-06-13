813368f58c75cbe0d526c767ff16c111f07fbf0e
Lior Sinai <sinailior>
2018-06-13 17:07:10 +0200

    corrected Hmatrices names

--- getHmatrices.m
+++ getHmatrices.m
@@ -3,7 +3,7 @@
 %%% Calculate the end effector position using Brockett's formula
 
 
-function [H1_0, H2_0, H3_0] = getHmatrices(q, L)
+function [H0_1, H0_2, H0_3] = getHmatrices(q, L)
     
     %Define unit twists
     omega_unit=[0 0 1]'; r=[0 0 0]';
@@ -21,9 +21,9 @@ function [H1_0, H2_0, H3_0] = getHmatrices(q, L)
     H3_0_0=[eye(3) [0 0 L(1)+L(2)+L(3)]' ; 0 0 0 1];
     
    % Calculate the H matrices
-    H1_0 = expTwist(T100,q(1))*H1_0_0;
-    H2_0 = expTwist(T100,q(1))*expTwist(T210,q(2))*H2_0_0;
-    H3_0 = expTwist(T100,q(1))*expTwist(T210,q(2))*expTwist(T320,q(3))*H3_0_0; 
+    H0_1 = expTwist(T100,q(1))*H1_0_0;
+    H0_2 = expTwist(T100,q(1))*expTwist(T210,q(2))*H2_0_0;
+    H0_3 = expTwist(T100,q(1))*expTwist(T210,q(2))*expTwist(T320,q(3))*H3_0_0; 
 
 end
 
