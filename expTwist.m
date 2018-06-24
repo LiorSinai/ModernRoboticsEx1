cb7f20cd19ea96d4da3221636fde79f6bcf76df1
Lior Sinai <sinailior>
2018-06-24 17:18:51 +0200

    added pure translation case where omega=0

--- expTwist.m
+++ expTwist.m
@@ -6,12 +6,16 @@ function H= expTwist(T,q)
     omega=T(1:3);
     v    =T(4:6);
     
-    R=rodriguez(omega,q) ;
-    
-    %omega=q*omega;
-    %v=q*v;
-    
-    H=[R ((eye(3)-R)*(cross(omega,v))+q*omega'*v*omega)/norm(omega)^2; 0 0 0 1];
+    if norm(omega)~=0
+        R=rodriguez(omega,q) ;
+        %omega=q*omega;
+        %v=q*v;
+        H=[R ((eye(3)-R)*(cross(omega,v))+q*omega'*v*omega)/norm(omega)^2; 0 0 0 1];
+        %note: invalid if norm(omega)=0
+    else
+        % a translation only
+        H=[eye(3) v*q; 0 0 0 1]; 
+    end
     
 end
 
